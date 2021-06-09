0ace5679ec658ed4d6c19cbb357cd526095808e0
rabat1 <57858448+rabat1@users.noreply.github.com>
2021-06-09 21:36:04 +0500

    shopping-app-amazona

---
+++ backend/data.js
@@ -0,0 +1,51 @@
+export default  {
+    products:[
+        {
+            _id:1,
+            countInStock:0,
+            category:'shirts',
+            name:'fit-shirt',
+            image:'https://johnlewis.scene7.com/is/image/JohnLewis/005103465',
+            price: 60,
+            brand:'nike',
+            rating: 6.5,
+            numReviews:10,
+            },
+            {
+                _id:2,
+                countInStock:4,
+                category:'shirts',
+                name:'red-shirt',
+                image:'https://5.imimg.com/data5/RL/CB/WK/SELLER-8866612/formal-shirt-pant-500x500.jpg',
+                price: 60,
+                brand:'nike',
+                rating: 6.5,
+                numReviews:10,
+                
+                },
+            {
+                _id:3,
+                
+            countInStock:5,
+                category:'shirts',
+                name:'white-shirt',
+                image:'https://cdn-skin.sanaullastore.com/media/catalog/product/cache/1/small_image/271x378/9df78eab33525d08d6e5fb8d27136e95/b/r/brumano-junior-shirts-collection-2021-bm20js-blue-_-red-checkered-casual-shirt-junior-_1_.jpg',
+                price: 50,
+                brand:'uyt',
+                rating: 7.5,
+                numReviews:6,
+                
+                },
+                {
+                    _id:4,
+                    countInStock:6,
+                    category:'paints',
+                    name:'Paint',
+                    image:'https://oldnavy.gapcanada.ca/webcontent/0018/561/648/cn18561648.jpg',
+                    price: 60,
+                    brand:'jjj',
+                    rating: 6,
+                    numReviews:2,
+                                  },
+                                   ]
+}
